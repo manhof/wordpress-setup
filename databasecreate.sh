613371437af85860b4ab3e5b53f48ac5c5dc580b
manhof <manhof@users.noreply.github.com>
2017-06-30 11:15:50 -0400

    Update databasecreate.sh

--- databasecreate.sh
+++ databasecreate.sh
@@ -12,9 +12,9 @@ if [[ "$localdb" = true ]]
  then
  echo "setting up the local db" >> /home/test
  echo "username is wordpress, password is $wppass, database is $database" >> /home/test
- mysql -u root  -p$dbrootpw -e "CREATE DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
- mysql -u root  -p$dbrootpw -e "GRANT ALL ON $database.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '$wppass';"
- mysql -u root  -p$dbrootpw -e "Flush PRIVILEGES;"
+ mysql -u root  -p"$dbrootpw" -e "CREATE DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+ mysql -u root  -p"$dbrootpw" -e "GRANT ALL ON $database.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '$wppass';"
+ mysql -u root  -p"$dbrootpw" -e "Flush PRIVILEGES;"
 else
  mysql -h $dbserver -u root  -p$dbrootpw -e "CREATE DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
  mysql -h $dbserver -u root  -p$dbrootpw -e "GRANT ALL ON $database.* TO 'wordpressuser'@'%' IDENTIFIED BY '$wppass';"
