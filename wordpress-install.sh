3a4382a2d73140cd9c2fa13c95304919fb44caf7
manhof <manhof@users.noreply.github.com>
2017-06-30 12:00:28 -0400

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -48,7 +48,7 @@ fi
 echo "define('DB_CHARSET', 'utf8');" >> /var/www/html/wp-config.php 
 echo "define('DB_COLLATE', '');" >> /var/www/html/wp-config.php
 cat /home/salt.txt >> /var/www/html/wp-config.php
-echo "$table_prefix  = 'wp_';" >> /var/www/html/wp-config.php
+echo "\$table_prefix  = 'wp_';" >> /var/www/html/wp-config.php
 echo "if ( !defined('ABSPATH') )" >> /var/www/html/wp-config.php
 echo "        define('ABSPATH', dirname(__FILE__) . '/');" >> /var/www/html/wp-config.php
 echo "require_once(ABSPATH . 'wp-settings.php');" >> /var/www/html/wp-config.php
